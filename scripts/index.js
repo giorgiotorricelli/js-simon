c609cde4819e132c244292a371834dc51ffc4b3f
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-07 17:46:24 +0200

    generazione numeri casuali al click del tasto

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,40 @@
+const memoNumbers = document.querySelectorAll('.memo-input');
+const generatorBtn = document.querySelector('.generate-numb');
+const memoArray = [];
+const scrittaTempo = document.querySelector('.timer');
+
+function numberGenerator() {
+    memoArray.length = 0;
+
+    while (memoArray.length < memoNumbers.length) {
+        let currentValue = Math.ceil(Math.random() * 99);
+        if (!memoArray.includes(currentValue)) {
+            memoArray.push(currentValue);
+        }
+    }
+
+    for (let i = 0; i < memoNumbers.length; i++) {
+        memoNumbers[i].value = memoArray[i];
+    }
+
+    setInterval(
+
+    )
+
+}
+
+
+function countDown() {
+    
+}
+
+
+
+
+
+generatorBtn.addEventListener(
+    'click', numberGenerator
+);
+
+
 
